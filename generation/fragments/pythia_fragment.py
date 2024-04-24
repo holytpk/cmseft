5f9d2bac957b8a7f7f11cdaddefbb2a9ba398d15
Lingqiang He <he614@purdue.edu>
2024-04-23 22:21:42 -0500

    test for TT01j2lCA

--- generation/fragments/pythia_fragment.py
+++ generation/fragments/pythia_fragment.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
-    args = cms.vstring('/eos/uscms/store/user/dspitzba/TT01j_tutorial_slc7_amd64_gcc700_CMSSW_10_6_19_tarball.tar.xz'),
+    args = cms.vstring('/uscms_data/d3/he614/cmseft/CA/TT01j2lCA_slc7_amd64_gcc700_CMSSW_10_6_19_tarball.tar.xz'),
     nEvents = cms.untracked.uint32(5000),
     numberOfParameters = cms.uint32(1),
     outputFile = cms.string('cmsgrid_final.lhe'),
